e1964e38c297409cca51eb05380875adb6dbdbca
T4GAD4 <capi.aurelien@gmail.com>
2015-06-18 11:32:49 +0200

    Mise en ligne de la page Params de l'admin et séparation du menu dans le template au lieu de le mettre dans chaque page!

--- appli/controllers/walkadmin/dashboard.php
+++ appli/controllers/walkadmin/dashboard.php
@@ -1,36 +1,39 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
 
 class Dashboard extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -  
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in 
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see http://codeigniter.com/user_guide/general/urls.html
-	 */
-	public function index()
-	{
-            connecte_admin($this->session->userdata('admin'));
-            $data = array();
-            $data['commentaires'] = $this->commentaires->count_non_modere();
-            $data['reservations'] = $this->reservations->count_en_cours();
-            $data['admin'] = $this->session->userdata('admin');
-            $this->load->view('wadmin/template/header');
-            $this->load->view('wadmin/dashboard',$data);
-            $this->load->view('wadmin/template/footer');
-            //$this->output->enable_profiler(true);
-	}
-}
+    /**
+     * Index Page for this controller.
+     *
+     * Maps to the following URL
+     * 		http://example.com/index.php/welcome
+     * 	- or -  
+     * 		http://example.com/index.php/welcome/index
+     * 	- or -
+     * Since this controller is set as the default controller in 
+     * config/routes.php, it's displayed at http://example.com/
+     *
+     * So any other public methods not prefixed with an underscore will
+     * map to /index.php/welcome/<method_name>
+     * @see http://codeigniter.com/user_guide/general/urls.html
+     */
+    public function index() {
+        connecte_admin($this->session->userdata('admin'));
+        $data = array();
+        $data['commentaires'] = $this->commentaires->count_non_modere();
+        $data['reservations'] = $this->reservations->count_en_cours();
+        $data['admin'] = $this->session->userdata('admin');
+        $this->load->view('wadmin/template/header');
+        $this->load->view('wadmin/template/menu', $data);
+        $this->load->view('wadmin/dashboard');
+        $this->load->view('wadmin/template/footer');
+        //$this->output->enable_profiler(true);
+    }
 
+}
 
 /* End of file accueil.php */
 /* Location: ./application/controllers/accueil.php */
