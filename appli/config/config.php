39aaca46faf285607f5fc30a4b6d56fc09f75c79
T4GAD4 <capi.aurelien@gmail.com>
2015-06-18 11:14:27 +0200

    Version AurélienModification du nom pour mon local

--- appli/config/config.php
+++ appli/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/Walkabout_local/';
+$config['base_url']	= 'http://localhost/walkabout-voyage/';
 
 /*
 |--------------------------------------------------------------------------
