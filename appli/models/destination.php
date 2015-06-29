79ee01a60fabe4fe8addfcce869fb6bd2377b192
S4nG0 <julien.vdm@gmail.com>
2015-06-29 12:04:29 +0200

    Changed travels url encoding

--- appli/models/destination.php
+++ appli/models/destination.php
@@ -6,49 +6,49 @@ if (!defined('BASEPATH'))
 class Destination extends CI_Model {
 
     protected $table = 'destination';
-    
-    
-    
+
+
+
     public function constructeur($id = 0){
         if($id == 0){
             return false;
         }
-        
+
         $destination = $this->db->select('*')
                            ->from($this->table)
                            ->where('idDestination', $id)
                            ->limit(1)
                            ->get()
                            ->result();
-        
+
         return $destination;
-    } 
-    
+    }
+
     public function getFromName($name = ''){
         if($name == ''){
             return false;
         }
-        
-        $name = urldecode($name);
-        
+
+        $name = str_replace('-', ' ', $name);
+
         $destination = $this->db->select('*')
                            ->from($this->table)
                            ->like('titre', "$name")
                            ->limit(1)
                            ->get()
                            ->result();
-        
+
         return $destination;
-    }  
-    
+    }
+
     public function get_all(){
-        
+
         $destinations = $this->db->select('*')
                            ->from($this->table)
                            ->get()
                            ->result();
-        
+
         return $destinations;
     }
-    
+
 }
