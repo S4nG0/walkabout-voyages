9a2c05f516149608b3e0ef0b40472513b62e54fd
S4nG0 <julien.vdm@gmail.com>
2015-06-29 16:33:07 +0200

    Ranking modifications

--- appli/views/accueil.php
+++ appli/views/accueil.php
@@ -32,7 +32,11 @@ $page = "home";
 
                     <div class="col-md-8 col-md-offset-2">
 
-                        <h1 class="no-sep">Savoir, échanger, partager et découvrir</h1>
+                        <?php if ($page == "home") : ?>
+                        <h1 class="hidden">Walkabout</h1>
+                        <?php endif; ?>
+
+                        <p class="lead">Savoir, échanger, partager et découvrir</p>
 
                         <p>S'envoler vers des terres reculées<br />vivre une expérience inoubliable</p>
 
