b2a36aa08d0dc332d173a7025612d48ce4a4822d
S4nG0 <julien.vdm@gmail.com>
2015-06-30 15:23:13 +0200

    Modif-carnet.php changes

--- appli/views/modif_carnet.php
+++ appli/views/modif_carnet.php
@@ -9,6 +9,7 @@ $page = "moncompte";
             <!-- Navbar -->
             <?php include 'template/menu.php'; ?>
         </div>
+<<<<<<< HEAD
 
         <div class="row noPadding">
             <div class="big-title-wrapper">
@@ -29,4 +30,10 @@ $page = "moncompte";
     <div class="content">
 
 
+=======
+        
+    </div>
+
+    <div class="content">
+>>>>>>> 72c9228d0cfddf85af104057e19c298c3c410559
     </div>
