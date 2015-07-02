f8666f2d1d57bedb48fd3d297a5446112ab451b6
root <lamootalexandre@gmail.com>
2015-07-02 11:23:12 +0200

    fonctionnalité liste et modif destination

---
+++ appli/views/wadmin/ListeDestination.php
@@ -0,0 +1,38 @@
+
+<div id="wrapper">
+    <div class="row">
+        <div class="col-lg-12">
+            <h1 class="page-header">Liste des destinations</h1>
+        </div>
+        <!-- /.col-lg-12 -->
+    </div>
+    <!-- /.row -->
+    <div class="row">
+        <table style="margin-left: 2%">
+            <thead>
+                <tr>
+                    <td>Titre</td>
+                    <td>Nom du voyage</td>
+                    <td style="width: 30%">Description</td>
+                    <td>Pays</td>
+                    <td>Ville</td>
+                    <td>Modifier la destination</td>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach($pays as $key=>$value){?>
+                    <tr>
+                        <td><?php echo $pays[$key]->titre ?></td>
+                        <td><?php echo $pays[$key]->nom ?></td>
+                        <td style="width: 30%"><?php echo $pays[$key]->description ?></td>
+                        <td><?php echo $pays[$key]->nom ?></td>
+                        <td><?php echo $pays[$key]->ville ?></td>
+                        <td><input type="button" value="Modifier destination" onclick="document.location='<?php echo base_url()."walkadmin/destinationadmin/detailTravel/".$pays[$key]->idDestination ?>'"></td>
+                    </tr>
+                <?php
+                    }
+                ?>
+            </tbody>
+        </table>
+    </div>
+</div>
