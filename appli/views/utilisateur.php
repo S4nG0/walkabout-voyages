55a2393ebc1766b8cad549a53b4a2e2e265f19ed
S4nG0 <julien.vdm@gmail.com>
2015-07-19 14:53:57 +0200

    Added user controller & view

---
+++ appli/views/utilisateur.php
@@ -0,0 +1,17 @@
+<?php
+$page = "page-utilisateur";
+?>
+
+<body class="page-utilisateur">
+
+    <div class="main banner" id="main">
+        <div class="container-fluid noPadding">
+            <!-- Navbar -->
+            <?php include 'template/menu.php'; ?>
+
+            <div class="big-title-wrapper">
+                <div class="big-title">
+                    <h1 class="no-sep">Les carnets de nos utilisateurs</h1>
+                </div>
+            </div>
+        </div>
