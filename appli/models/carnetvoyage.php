ef4061832401364f11f805983653abcd4401ded5
T4GAD4 <capi.aurelien@gmail.com>
2015-07-19 12:09:07 +0200

    Adding Map on page pays_admin

--- appli/models/carnetvoyage.php
+++ appli/models/carnetvoyage.php
@@ -6,24 +6,24 @@ if (!defined('BASEPATH'))
 class Carnetvoyage extends CI_Model {
 
     protected $table = 'carnetdevoyage';
-    
-    
+
+
     public function constructeur($id = 0){
         if($id == 0){
             return false;
         }
-        
+
         $carnet = $this->db->select('*')
                            ->from($this->table)
                            ->where('idCarnetDeVoyage', $id)
                            ->limit(1)
                            ->get()
                            ->result();
-        
+
         return $carnet;
     }
-    
-    
+
+
     public function get_carnets_alea($idDestination = 0){
         if($idDestination != 0){
             $carnets = $this->db->select('*')
@@ -43,11 +43,32 @@ class Carnetvoyage extends CI_Model {
                                ->get()
                                ->result();
         }
-        
+
         return $carnets;
-        
+
     }
-    
+
+
+    public function get_carnets($idDestination = 0){
+        if($idDestination != 0){
+            $carnets = $this->db->select('*')
+                               ->from($this->table)
+                               ->where('publie', 'true')
+                               ->where('idDestination', $idDestination)
+                               ->get()
+                               ->result();
+        }else{
+            $carnets = $this->db->select('*')
+                               ->from($this->table)
+                               ->where('publie', 'true')
+                               ->get()
+                               ->result();
+        }
+
+        return $carnets;
+
+    }
+
     public function getFromName($name = ''){
         if($name == ''){
             return false;
@@ -62,31 +83,31 @@ class Carnetvoyage extends CI_Model {
 
         return $destination;
     }
-    
-    
+
+
     public function get_carnet_pagination($start,$nb){
-        
+
         $carnets = $this->db->select('*')
                            ->from($this->table)
                            ->where('publie', 'true')
                            ->limit($nb, $start)
                            ->get()
-                           ->result(); 
-            
+                           ->result();
+
         return $carnets;
-        
+
     }
-    
+
     public function get_carnet_for_user($id=''){
-        
+
         $carnets = $this->db->select('*')
                            ->from($this->table)
                            ->where('idUsers', $id)
                            ->get()
-                           ->result(); 
-            
+                           ->result();
+
         return $carnets;
-        
+
     }
 
     public function get_carnet_for_user_join_destination($id=''){
@@ -100,16 +121,16 @@ class Carnetvoyage extends CI_Model {
 
         return $carnets;
     }
-    
+
     public function modify($data = '', $id = 0){
-        
+
         if($data == '' || $id == 0){
             return false;
         }
         $result =    $this->db->where('idCarnetDeVoyage', $id);
-                     $this->db->update($this->table, $data); 
-                     
+                     $this->db->update($this->table, $data);
+
         return $result;
     }
-    
+
 }
