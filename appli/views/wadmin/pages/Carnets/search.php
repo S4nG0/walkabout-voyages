11c8b2dc358a6c9268212b8b86b59595c2bbb18f
T4GAD4 <capi.aurelien@gmail.com>
2015-08-09 22:13:42 +0200

    Correction design div non ferme

--- appli/views/wadmin/pages/Carnets/search.php
+++ appli/views/wadmin/pages/Carnets/search.php
@@ -12,12 +12,12 @@
     <div class="row">
         <div class="col-md-12">
             <div class="module__tools">
-                <?php echo form_open('walkadmin/carnets/search'); ?>
                 <div class="custom-search">
+                <?php echo form_open('walkadmin/carnets/search'); ?>
                     <input class="custom-search-input" type="search" name="search" placeholder="Rechercher" value="<?php echo $search; ?>"/>
                     <button class="custom-search-button"><i class="fa fa-search"></i></button>
-                </div>
                 <?php echo form_close(); ?>
+                </div>
                 <a href="<?php echo base_url() . 'walkadmin/carnets/supprimes'; ?>" class="button black"><i class="fa fa-trash"></i>&nbsp;Corbeille</a>
             </div>
         </div>
